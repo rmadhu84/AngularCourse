11732783c034b6859140b8df6992d98489fecceb
rmadhu84 <r.madhu.84@gmail.com>
2021-11-16 11:18:35 -0500

    Added address card component

---
+++ src/app/address-card/address-card.component.ts
@@ -0,0 +1,21 @@
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from "@angular/core";
+import { User } from "./user.model";
+
+@Component({
+  selector: "app-address-card",
+  templateUrl: "./address-card.component.html",
+  styleUrls: ["./address-card.component.css"],
+})
+export class AddressCardComponent implements OnInit {
+  @Input("user2") user: User;
+  @Input("state") state: string;
+  @Input("zip") zip: number;
+  constructor() {}
+  ngOnInit() {}
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { User } from "./address-card/user.model";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'second-project';
+  inputText: string = "Initial Value";
+  inputUser: User;
+  constructor() {
+    this.inputUser = new User();
+    this.inputUser.name = "Madhu Ramachandran";
+    this.inputUser.designation = "Software Developer";
+    this.inputUser.address = "2350, Club Meridian Dr, A06";
+    this.inputUser.phone = ["517-507-9422", "517-455-9202"];
+  }
 }
